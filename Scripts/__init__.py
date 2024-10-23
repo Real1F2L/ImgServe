adf618fcdadcc5d07e28decacc6543f51782872a
1f2l <aidencabreja@gmail.com>
2024-10-22 20:49:44 -0400

    Added trycatch to /downloads

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -172,53 +172,61 @@ async def downloadFile(request:Request):
     Returns:
         File response
     """
-    requestQuery = request.query_params
-    if (not requestQuery): raise HTTPException(status_code=400, detail="Missing query")
+    try:
 
-    _file = os.path.join(utils._root, unquote(requestQuery.get("file", ""))) #? The location of the file to return
-    _imgSize = int(unquote(requestQuery.get("size", 64))) #? the image size, default 64
-    _extensionWithoutDot = os.path.splitext(_file)[1].split(".")[1].lower() #? the file extension
-    _basename = os.path.basename(_file) #? the entire name of the file (including extension)
-    _CacheSavePath = os.path.join(utils._CacheLocation, _basename + "_resized" + unquote(requestQuery.get("size", "64")) + ".webp") #? where the image will be stored in the cache
+        requestQuery = request.query_params
+        if (not requestQuery): raise HTTPException(status_code=400, detail="Missing query")
 
-    for name in utils._dontShareNames:
-        if name in _file:
-            raise HTTPException(status_code=400, detail="Access to this file is denied.")
+        _file = os.path.join(utils._root, unquote(requestQuery.get("file", ""))) #? The location of the file to return
+        _imgSize = int(unquote(requestQuery.get("size", 64))) #? the image size, default 64
+        _extensionWithoutDot = os.path.splitext(_file)[1].split(".")[1].lower() #? the file extension
+        _basename = os.path.basename(_file) #? the entire name of the file (including extension)
+        _CacheSavePath = os.path.join(utils._CacheLocation, _basename + "_resized" + unquote(requestQuery.get("size", "64")) + ".webp") #? where the image will be stored in the cache
 
-    # checks
-    _exists = os.path.exists(_file)
-    _isFile = os.path.isfile(_file)
+        for name in utils._dontShareNames:
+            if name in _file:
+                raise HTTPException(status_code=400, detail="Access to this file is denied.")
 
-    #! reject access to other folders
-    if not _file.startswith(utils._root):
-        raise HTTPException(status_code=400, detail="Access to this folder is denied.")
+        # checks
+        _exists = os.path.exists(_file)
+        _isFile = os.path.isfile(_file)
 
-    if not _exists:
-        raise HTTPException(status_code=404, detail="File does not exist or is a folder")
-    elif not _isFile:
-        raise HTTPException(status_code=400, detail="Path is a folder")
-    
-    else:
-        img_files = ["png", "jpg", "jpeg", "webp", "svg", "heic", "pdn"]
+        #! reject access to other folders
+        if not _file.startswith(utils._root):
+            raise HTTPException(status_code=400, detail="Access to this folder is denied.")
 
-        # check if the file is an image
-        if (_extensionWithoutDot in img_files):
-            # check if the image is already in cache
-            if (os.path.exists(_CacheSavePath)):
-                return FileResponse(_CacheSavePath, filename=_basename, media_type="image/webp", status_code=200)
-
-            # open the image with PIL
-            with Image.open(_file) as img:
-                # resize the image to the size specified
-                img.thumbnail((_imgSize, _imgSize), Image.Resampling.LANCZOS)
-                img.save(_CacheSavePath, "webp") #? save image to cache
-
-                # return the new resized image
-                return FileResponse(_CacheSavePath, filename=os.path.basename(_CacheSavePath), media_type="image/webp", status_code=200)
-
-        # not an image, just return the file
+        if not _exists:
+            raise HTTPException(status_code=404, detail="File does not exist or is a folder")
+        elif not _isFile:
+            raise HTTPException(status_code=400, detail="Path is a folder")
+        
         else:
-            return FileResponse(_file, filename=_basename, status_code=200)
+            img_files = ["png", "jpg", "jpeg", "webp", "svg", "heic", "pdn"]
+
+            # check if the file is an image
+            if (_extensionWithoutDot in img_files):
+                # check if the image is already in cache
+                if (os.path.exists(_CacheSavePath)):
+                    return FileResponse(_CacheSavePath, filename=_basename, media_type="image/webp", status_code=200)
+
+                # open the image with PIL
+                with Image.open(_file) as img:
+                    # resize the image to the size specified
+                    img.thumbnail((_imgSize, _imgSize), Image.Resampling.LANCZOS)
+                    img.save(_CacheSavePath, "webp") #? save image to cache
+
+                    # return the new resized image
+                    return FileResponse(_CacheSavePath, filename=os.path.basename(_CacheSavePath), media_type="image/webp", status_code=200)
+
+            # not an image, just return the file
+            else:
+                return FileResponse(_file, filename=_basename, status_code=200)
+            
+    except Exception as e:
+        print(f"{utils.color.r}[download] Unexpected exception: {e}.{utils.color.re}")
+        utils._RequestLogger.error(f"Unexpected exception: {e}. Traceback will be logged in ErrorLogs.log")
+        utils._ErrorLogger.exception(f"Unexpected exception: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 # Display video, return a video in a lower resoluton and bitrate
 @utils.fastAPI.get("/display-video")
